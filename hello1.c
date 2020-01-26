685a6a4d1ad3a6aab537dfbaa1d1e99aac4b3dcf
Asami990 <54513398+Asami990@users.noreply.github.com>
2020-01-25 20:09:38 -0600

    Update hello1.c
    add my name and major

--- hello1.c
+++ hello1.c
@@ -2,7 +2,10 @@
 
 int main (void){
   printf("yousuf alsalmi");
-  printf("electrical engineering")
+  printf("electrical engineering");
+  
+  printf("Osama Al Sabahi");
+  printf("Electrical Engineering");
 
   return 0;
 }
